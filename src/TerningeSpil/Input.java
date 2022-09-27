fd7101b3bd5da60a44c0c2fa83ab9174e1f7c4ae
fo123g <58248701+fo123g@users.noreply.github.com>
2022-09-27 09:52:32 +0200

    Made Input class with enter method

---
+++ src/TerningeSpil/Input.java
@@ -0,0 +1,13 @@
+package TerningeSpil;
+
+import java.util.Scanner;
+
+public class Input {
+    public static String enter()
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        return s;
+    }
+
+}
