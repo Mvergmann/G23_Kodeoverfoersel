daadb49035c058e3c855bc971684fd066ea56357
Mvergmann <mathiasvergmann@gmail.com>
2022-09-27 10:13:47 +0200

    CDIO version 1, uden enter input funktion

--- src/TerningeSpil/TerningeKast.java
+++ src/TerningeSpil/TerningeKast.java
@@ -31,20 +31,6 @@ public class TerningeKast {
             point1 += sum1;
             System.out.println("du har nu: " + point1 + " point.");
 
-            if (kastsp1.getDie1Value() == 1 && kastsp1.getDie2Value() == 1){
-                System.out.println("Du har slået to, 1'ere og mister alle dine point");
-                point1 = 0;
-
-            }
-
-            while (kastsp1.getDie1Value() == kastsp1.getDie2Value()) {
-                System.out.println(spiller1 + " slog 2 ens! Kast igen! ");
-                kastsp1.tur();
-                sum1 = kastsp1.sum;
-                System.out.println("du har slået " + kastsp1.getDie1Value() + " og " + kastsp1.getDie2Value());
-                point1 += sum1;
-                System.out.println("du har nu: " + point1 + " point.");
-
             }
             System.out.println(spiller2 + " kaster terningerne");
             kastsp2.tur();
@@ -53,21 +39,6 @@ public class TerningeKast {
             point2 += sum2;
             System.out.println("du har nu: " + point2 + " point");
 
-            if (kastsp2.getDie1Value() == 1 && kastsp2.getDie2Value() == 1) {
-                System.out.println("Du har slået to, 1'ere og mister alle dine point");
-                point2 = 0;
-            }
-
-            while (kastsp2.getDie1Value() == kastsp2.getDie2Value()) {
-                System.out.println(spiller2 + " slog to ens, kast igen!");
-                kastsp2.tur();
-                sum2 = kastsp2.sum;
-                System.out.println("du har slået " + kastsp2.getDie1Value() + " og " + kastsp2.getDie2Value());
-                point2 += sum2;
-                System.out.println("du har nu: " + point2 + " point");
-            }
-
-            }
         if (point1 > 40) {
             System.out.println(spiller1 + " har vundet!");
             System.out.println("Scoren blev: " + spiller1 + " point og " + spiller2 + " point");
