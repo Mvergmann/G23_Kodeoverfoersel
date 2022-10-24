bfa5673ba2a206146581bb3cd49459d75282a314
nikl2513 <nikl2513@outlook.dk>
2022-10-24 16:28:43 +0200

    tilføjet player class

--- src/Feltspil/Player.java
+++ src/Feltspil/Player.java
@@ -1,4 +1,23 @@
 package Feltspil;
 
+import java.util.Scanner;
+
 public class Player {
+    private int saldo = 0;
+
+    private String navn;
+
+    public Player (String navn){
+        Scanner plna = new Scanner(System.in);
+        navn = plna.nextLine();
+
+    }
+    public int GetSaldo (int saldo){
+        return saldo;
+    }
+
+    public int AddToSaldo (int ammount){
+        saldo = saldo + ammount;
+        return saldo;
+    }
 }
