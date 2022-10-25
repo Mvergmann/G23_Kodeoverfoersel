67502b0557551d6cac3ddba88dc517dfe2d66ac8
Mvergmann <mathiasvergmann@gmail.com>
2022-10-25 12:15:25 +0200

    Changed language to english partially

--- src/Feltspil/main.java
+++ src/Feltspil/main.java
@@ -2,17 +2,16 @@ package Feltspil;
 
 import TerningeSpil.Input;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("welcome to the game");
-        System.out.println("what is the first players name?");
+        System.out.println("Please enter the name of the first players: ");
         String navn1 = scan.nextLine();
         Player player1 = new Player(navn1, 0);
-        System.out.println("what is the second players name?");
+        System.out.println("Please enter the name of the first players: ");
         String navn2 = scan.nextLine();
         Player player2 = new Player(navn2, 0);
         System.out.println("welcome to the game " + player1.toString() + " and " + player2.toString());
@@ -22,45 +21,48 @@ public class main {
         Language Langmain = new Language();
 
         while (player1.GetSaldo() < 3000 && player2.GetSaldo() < 3000) {
-            System.out.println(navn1 + " starter med at slå terningerne, tryk enter for at slå");
+            System.out.println("it is " + navn1 + "'s turn, press enter to roll the dice");
             Input.enter();
             int roll1 = p1tur.tur();
-            System.out.println("du har slået " + roll1);
-            System.out.println("det vil sige du lander på felt " + Tlmain.getTitle(roll1));
+            System.out.println("You rolled " + roll1);
+            System.out.println("You landed on the tile: " + Tlmain.getTitle(roll1));
             System.out.println(Tlmain.getFlavourText(roll1));
-            System.out.println("du har fået" + Tlmain.getValue(roll1));
+            System.out.println("You have gained " + Tlmain.getValue(roll1) + " points.");
             player1.AddToSaldo(Tlmain.getValue(roll1));
-            System.out.println("du er nu op på" + player1.GetSaldo() + " point");
+            System.out.println("Your total is now " + player1.GetSaldo() + " point.");
             if (roll1 == 10) {
                 System.out.println(Langmain.extraturn());
+                Input.enter();
                 roll1 = p1tur.tur();
-                System.out.println("du har slået " + roll1);
-                System.out.println("det vil sige du lander på felt " + Tlmain.getTitle(roll1));
+                System.out.println("You rolled " + roll1);
+                System.out.println("You landed on the tile: " + Tlmain.getTitle(roll1));
                 System.out.println(Tlmain.getFlavourText(roll1));
-                System.out.println("du har fået" + Tlmain.getValue(roll1));
+                System.out.println("You have gained " + Tlmain.getValue(roll1) + " points.");
                 player1.AddToSaldo(Tlmain.getValue(roll1));
-                System.out.println("du er nu op på" + player1.GetSaldo() + " point");
+                System.out.println("Your total is now " + player1.GetSaldo() + " point.");
             }
 
 
-            System.out.println(navn2 + " starter med at slå terningerne, tryk enter for at slå");
+            System.out.println("it is " + navn2 + "'s turn, press enter to roll the dice");
             Input.enter();
             int roll2 = p2tur.tur();
-            System.out.println("du har slået " + roll2);
-            System.out.println("det vil sige du lander på felt " + Tlmain.getTitle(roll2));
+            System.out.println("You rolled " + roll2);
+            System.out.println("You landed on the tile: " + Tlmain.getTitle(roll2));
             System.out.println(Tlmain.getFlavourText(roll2));
-            System.out.println("du har fået" + Tlmain.getValue(roll2));
+            System.out.println("You have gained " + Tlmain.getValue(roll2) + " points.");
             player2.AddToSaldo(Tlmain.getValue(roll2));
-            System.out.println("du er nu op på" + player2.GetSaldo() + " point");
+            System.out.println("Your total is now " + player2.GetSaldo() + " point.");
+
             if (roll2 == 10) {
                 System.out.println(Langmain.extraturn());
+                Input.enter();
                 roll2 = p2tur.tur();
-                System.out.println("du har slået " + roll2);
-                System.out.println("det vil sige du lander på felt " + Tlmain.getTitle(roll2));
+                System.out.println("You rolled " + roll2);
+                System.out.println("You landed on the tile: " + Tlmain.getTitle(roll2));
                 System.out.println(Tlmain.getFlavourText(roll2));
-                System.out.println("du har fået" + Tlmain.getValue(roll2));
+                System.out.println("You have gained " + Tlmain.getValue(roll2) + " points.");
                 player2.AddToSaldo(Tlmain.getValue(roll2));
-                System.out.println("du er nu op på" + player2.GetSaldo() + " point");
+                System.out.println("Your total is now " + player2.GetSaldo() + " point.");
             }
 
             if (player1.GetSaldo() > player2.GetSaldo()) {
