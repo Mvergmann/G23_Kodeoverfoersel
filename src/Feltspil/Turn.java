52e1e921b363dee145ce74f17d37ba8583eb2b2f
nikl2513 <nikl2513@outlook.dk>
2022-10-25 09:04:22 +0200

    tilføjet tur klasse fra terningespil

--- src/Feltspil/Turn.java
+++ src/Feltspil/Turn.java
@@ -1,4 +1,34 @@
 package Feltspil;
 
+import TerningeSpil.Die;
+
 public class Turn {
+    TerningeSpil.Die die1 = new TerningeSpil.Die();
+    TerningeSpil.Die die2 = new Die();
+    int sum;
+
+
+
+    public int tur() {
+        die1.roll();
+        die2.roll();
+        sum = die1.getFaceValue() + die2.getFaceValue();
+
+
+        return sum;
+
+    }
+
+    public int getDie1Value() {
+
+
+        return die1.getFaceValue();
+
+    }
+    public int getDie2Value(){
+        return die2.getFaceValue();
+
+
+
+    }
 }
