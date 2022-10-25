b3b02ed27da93688af921fc94c42b76b48eafff6
fo123g <58248701+fo123g@users.noreply.github.com>
2022-10-25 10:38:20 +0200

    Added title 2 and 3, Language constructor

--- src/Feltspil/Language.java
+++ src/Feltspil/Language.java
@@ -1,51 +1,65 @@
 package Feltspil;
 
 public class Language {
+
+    public Language(){
+
+    }
+
     /**
      * System.out.println methods for the flavour text that is shown when a player lands on a certain space.
      *
      */
-    public static String space2 (){
+
+    public static String title2(){
+        String title = "Tower";
+        return title;
+    }
+    public static String spacetext2 (){
         String space2 = " You visit the great tower and find a chest filled with ancient coins. ";
         return space2;
     }
-    public static String space3() {
+    public static String title3(){
+        String title = "Crater";
+        return title;
+    }
+    public static String spacetext3() {
         String space3 = " You fall into the great crater and is forced to pay for a rescue helicopter. ";
         return space3;
     }
-    public static String space4(){
+    public static String spacetext4(){
         String space4 = " At the great Palace gate you find a wallet filled with cash. ";
         return space4;
     }
-    public static String space5(){
+    public static String spacetext5(){
         String space5 = " Whilst wandering the cold desert you loose some of your money doing the long hike. ";
         return space5;
     }
-    public static String space6(){
+    public static String spacetext6(){
         String space6 = " You receive great gifts  from the people of the walled city. ";
         return space6;
     }
-    public static String space7(){
+    public static String spacetext7(){
         String space7 = " You spend the night as a guest at the monastery. ";
         return space7;
     }
-    public static String space8(){
+    public static String spacetext8(){
         String space8 = " You travel through the pitch black caves, in the darkness you drop some coins down a ravine never to be found. ";
         return space8;
     }
-    public static String space9(){
+    public static String spacetext9(){
         String space9 = " You find a pile of silver whilst making camp at the old mountain hut. ";
         return space9;
     }
-    public static String space10(){
+    public static String spacetext10(){
         String space10 = " You pay a visit to the mysterious Werewall, doing the night coins disapear from your wallet without a trace. ";
         return space10;
     }
-    public static String space11(){
+    public static String spacetext11(){
         String space11 = " You fall into a deep pit and must pay a crane operator to get you out. ";
     return space11;
     }
-    public static String space12(){
+    public static String spacetext12(){
         String space12 = " You stumble into an abandoned goldmine, in there you find mountains of gold left behind. ";
         return space12;
     }
