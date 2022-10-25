ddd5d07923cb7f3d02667a8da5691dcd0948d0e3
fo123g <58248701+fo123g@users.noreply.github.com>
2022-10-25 09:51:10 +0200

    Changed text to String variables instead of println statements, also added more flavour text for game start

--- src/Feltspil/Language.java
+++ src/Feltspil/Language.java
@@ -1,38 +1,84 @@
 package Feltspil;
 
 public class Language {
-    public void space2 (){
-        System.out.println("You visit the great tower and find a chest filled with ancient coins  ");
+    /**
+     * System.out.println methods for the flavour text that is shown when a player lands on a certain space.
+     *
+     */
+    public static String space2 (){
+        String space2 = " You visit the great tower and find a chest filled with ancient coins. ";
+        return space2;
     }
-    public void space3() {
-        System.out.println("You fall into the great crater and is forced to pay for a rescue helicopter");
+    public static String space3() {
+        String space3 = " You fall into the great crater and is forced to pay for a rescue helicopter. ";
+        return space3;
     }
-    public void space4(){
-        System.out.println("At the great Palace gate you find a wallet filled with cash");
+    public static String space4(){
+        String space4 = " At the great Palace gate you find a wallet filled with cash. ";
+        return space4;
     }
-    public void space5(){
-        System.out.println("Whilst wandering the cold desert you loose some of your money doing the long hike");
+    public static String space5(){
+        String space5 = " Whilst wandering the cold desert you loose some of your money doing the long hike. ";
+        return space5;
     }
-    public void space6(){
-        System.out.println("You receive great gifts  from the people of the walled city ");
+    public static String space6(){
+        String space6 = " You receive great gifts  from the people of the walled city. ";
+        return space6;
     }
-    public void space7(){
-        System.out.println("You spend the night as a guest at the monastery ");
+    public static String space7(){
+        String space7 = " You spend the night as a guest at the monastery. ";
+        return space7;
     }
-    public void space8(){
-        System.out.println("You travel through the pitch black caves, in the darkness you drop some coins down a ravine never to be found");
+    public static String space8(){
+        String space8 = " You travel through the pitch black caves, in the darkness you drop some coins down a ravine never to be found. ";
+        return space8;
     }
-    public void space9(){
-        System.out.println("You find a pile of silver whilst making camp at the old mountain hut");
+    public static String space9(){
+        String space9 = " You find a pile of silver whilst making camp at the old mountain hut. ";
+        return space9;
     }
-    public void space10(){
-        System.out.println("You pay a visit to the mysterious Werewall, doing the night coins disapear from your wallet without a trace");
-        System.out.println("The next day you wake up with renewed energy, you immediately take an extra turn");
+    public static String space10(){
+        String space10 = " You pay a visit to the mysterious Werewall, doing the night coins disapear from your wallet without a trace. ";
+        return space10;
     }
-    public void space11(){
-        System.out.println("You fall into a deep pit and must pay a crane operator to get you out");
+    public static String space11(){
+        String space11 = " You fall into a deep pit and must pay a crane operator to get you out. ";
+    return space11;
     }
-    public void space12(){
-        System.out.println("You stumble into an abandoned goldmine, in there you find mountains of gold left behind.");
+    public static String space12(){
+        String space12 = " You stumble into an abandoned goldmine, in there you find mountains of gold left behind. ";
+        return space12;
     }
+
+    /**
+     * Game start text methods
+     *
+     */
+    public static String welcome(){
+        String welcome = " Welcome to the game!";
+        return welcome;
+    }
+    public static String start(){
+        String start = " Press Enter to start the game. ";
+        return start;
+    }
+    public static String askname1(){
+
+        String askname1 = " Please type the name of player 1 ";
+        return askname1;
+    }
+    public static String askname2(){
+
+        String askname2 = " Please type the name of player 2 ";
+        return askname2;
+    }
+    public static String extraturn(){
+        String extraturn = " The next day you wake up with renewed energy, you immediately take an extra turn ";
+        return extraturn;
+    }
+    public static String and(){
+        String and = " and ";
+        return and;
+    }
+
 }
