84293d4794eed98b8dc5db475e750d03e0a15d16
Mvergmann <mathiasvergmann@gmail.com>
2022-10-26 09:17:48 +0200

    Comments on Tile complete

--- src/Feltspil/Tile.java
+++ src/Feltspil/Tile.java
@@ -6,12 +6,22 @@ public class Tile {
     private String flavourText;
     private int value;
 
-    public Tile(String title, String flavorText, int value) {
+    /**
+     * Makes a tile with a title, a flavour and a value.
+     *
+     */
+
+    public Tile(String title, String flavourText, int value) {
         this.title = title;
-        this.flavourText = flavorText;
+        this.flavourText = flavourText;
         this.value = value;
 
     }
+
+    /**
+     * Getters for the different parameters of the tile.
+     *
+     */
     public String getTitle() {
         return title;
     }
