d46d6c730c359b0b7d2d4dbb8ad2e23802af8472
MathiasLinde <113913029+MathiasLinde@users.noreply.github.com>
2022-10-26 23:52:29 +0200

    Added comments to the Unittest for negativ balance.

--- src/Feltspil/mainTest.java
+++ src/Feltspil/mainTest.java
@@ -6,31 +6,66 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class mainTest {
     @Test
-
     void positivebalance(){
+        /**
+         * Below are 4 variables that are used in the test
+         * STARTSALDO is the int value of the Test players bank
+         * navn1 is the  String that is set as "Test" in the scenario
+         * fail is the tallys up the times the balance goes below 0
+         * testamount counts how many times the test player has rolled the dice
+         */
         int STARTSALDO = 1000;
         String navn1 = "Test";
         int fail = 0;
-        int antalslag = 0;
-
+        int testamount = 0;
+        /**
+         * The following for loop runs a game turn a set amount of times in this case 50000
+         * T
+         */
         for (int i = 0; i < 50000;i++){
-           var player1 = new Player(navn1, STARTSALDO);
+
+            /**
+             * Here an instance of the classes Player, TileList, and Turn is created
+             */
+            var player1 = new Player(navn1, STARTSALDO);
             var Tlmain = new TileList();
             var p1tur = new Turn();
-            int roll1 = p1tur.tur();
 
+
+            /**
+             * These two lines roll the dice and update the balance based on what space on the gameboard(array)
+             * the player has landed on
+             */
+            int roll1 = p1tur.tur();
           int balance =  player1.AddToSaldo(Tlmain.getValue(roll1));
-        //int balance = -1;
+
+            /**
+             * The if loop checks that the balance is still a postive int
+             * if the balance < 0 the program will break from the for loop
+             */
             if(balance < 0){
                 System.out.println("Error negativ balance");
                 fail++;
                 break;
             }
-            antalslag++;
+            /**
+             * This incriments the testamount each time the loop finishes
+             */
+            testamount++;
         }
+        /**
+         * asserEquals checks the expected int of 0 failures where the balance goes below 0 and checks if it is equals
+         * the amount of times the if (balance < 0) has occured
+         */
         assertEquals(0,fail);
+
+        /**
+         * These lines merely print the amount of failures
+         * and the testamount
+         */
         System.out.println("Test has concluded " + fail + " fails");
-        System.out.println("Testet over " + antalslag);
+        System.out.println("Testet over " + testamount);
+
     }
 
 
